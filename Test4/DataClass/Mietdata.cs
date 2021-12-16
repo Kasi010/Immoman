4a5586827bca5f651e5f1f106e9bea500571af0b
Kristof Kasper <kristof-kasper@web.de>
2021-12-16 16:04:50 +0100

    Neu angelegt für Daten der Mietcalctabelle

---
+++ Test4/DataClass/Mietdata.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4.DataClass
+{
+    public class Mietdata
+    {
+        public decimal Kaltmiete { get; set; }
+        public decimal KaltmieteGesamt { get; set; }
+        public decimal SonstigeMietaufwendungen { get; set; }
+        public decimal UmlageBewirtschaftskosten { get; set; }
+        public decimal Heizkosten { get; set; }
+        public decimal KostenWassAbwasser { get; set; }
+        public decimal Nettokaltmiete { get; set; }
+        public decimal Warmmiete { get; set; }
+    }
+}
