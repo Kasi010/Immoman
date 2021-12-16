b13558fea571c6b2807e5786d202a9e9a0b9ba2a
Kristof Kasper <kristof-kasper@web.de>
2021-12-16 16:05:32 +0100

    Neu angelegt für Daten der Umlagecostcalctabelle

---
+++ Test4/DataClass/Umlagekostdata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4.DataClass
+{
+    public class Umlagekostdata
+    {
+        public decimal HausgeldNichtUmlage { get; set; }
+        public decimal Eigeninstandhaltungsrücklage { get; set; }
+        public decimal SonstigeNichtumlagekosten { get; set; }
+    }
+}
