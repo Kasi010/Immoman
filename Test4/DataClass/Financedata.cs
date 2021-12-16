ec4fd07b25024f9dce03da07fb362c8665a863f5
Kristof Kasper <kristof-kasper@web.de>
2021-12-16 16:04:01 +0100

    Neu angelegt für Daten der Financcalctabelle

---
+++ Test4/DataClass/Financedata.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4.DataClass
+{
+    public class Financedata
+    {
+        public decimal Darlehenssumme { get; set; }
+        public decimal Zinssatz { get; set; }
+        public decimal Tilgung { get; set; }
+    }
+}
