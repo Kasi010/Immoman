6fcae6992f7da0fbfe5e81e93c35bb1b9fe4e94e
Kristof Kasper <kristof-kasper@web.de>
2021-12-16 16:04:25 +0100

    Neu angelegt für Daten der Investcalctabelle

---
+++ Test4/DataClass/Investdata.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4.DataClass
+{
+    public class Investdata
+    {
+        public decimal Kaufpreis { get; set; }
+        public decimal Makler { get; set; }
+        public decimal Notar { get; set; }
+        public decimal Grundbuchamt { get; set; }
+        public decimal Grunderwerbsteuer { get; set; }
+        public decimal SummeKaufpreis { get; set; }
+        public decimal Anfangsinvestitionen { get; set; }
+    }
+}
