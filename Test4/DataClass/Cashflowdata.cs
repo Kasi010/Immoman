40d1016dd9758cd073e9139e75e96c03abbce1e8
Kristof Kasper <kristof-kasper@web.de>
2021-12-17 20:41:50 +0100

    Datenklasse für die berechneten Cashflowwerte und Kommentierung

---
+++ Test4/DataClass/Cashflowdata.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4.DataClass
+{
+    //Datenklasse für die abgefragten Daten zur Berechnung des Cashflows
+    public class Cashflowdata
+    {
+        public decimal Warmmiete { get; set; }
+        public decimal Bewirtschaftungskosten { get; set; }
+        public decimal Kapitaldienst { get; set; }
+    }
+}
